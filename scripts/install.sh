62bb6c6ef811be2ea46ad099d68d03c327890780
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-02-25 00:10:00 +0100

    newinstalls

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,21 +6,21 @@ sudo apt-get -y install zsh
 if type -p zsh > /dev/null; then
     echo "zsh Installed" >> $log_file
 else
-    echo "zsh FAILED TO INSTALL!" >> $log_file
+    echo "zsh failed to install!" >> $log_file
 fi
 
 sudo apt-get -y install curl
 if type -p curl > /dev/null; then
     echo "curl Installed" >> $log_file
 else
-    echo "curl FAILED TO INSTALL!" >> $log_file
+    echo "curl failed to install" >> $log_file
 fi
 
 sudo apt-get -y install docker.io
 if type -p docker.io > /dev/null; then
     echo "docker Installed" >> $log_file
 else
-    echo "docker FAILED TO INSTALL!" >> $log_file
+    echo "docker failed to install!" >> $log_file
 fi
 
 
@@ -28,28 +28,28 @@ sudo apt-get -y install herbstluftwm
 if type -p herbstluftwm > /dev/null; then
     echo "herbstluftwm Installed" >> $log_file
 else
-    echo "herbstluftwm FAILED TO INSTALL!" >> $log_file
+    echo "herbstluftwm failed to install!" >> $log_file
 fi
 
 sudo apt-get -y install npm
 if type -p npm > /dev/null; then
     echo "npm Installed" >> $log_file
 else
-    echo "npm FAILED TO INSTALL!" >> $log_file
+    echo "npm failed to install!" >> $log_file
 fi
 
 sudo apt-get -y install python-pip
 if type -p pip3 > /dev/null; then
     echo "pip Installed" >> $log_file
 else
-    echo "pip FAILED TO INSTALL!" >> $log_file
+    echo "pip failed to install!" >> $log_file
 fi
 
 sudo apt-get -y install bpython
 if type -p bpython > /dev/null; then
     echo "bpython Installed" >> $log_file
 else
-    echo "bpython FAILED TO INSTALL!" >> $log_file
+    echo "bpython failed to install!" >> $log_file
 fi
 
 sudo apt-get -y install bpython3
@@ -66,16 +66,10 @@ else
     echo "nodejs failed to install!" >> $log_file
 fi
 
+# Install SpaceVim
+curl -sLf https://spacevim.org/install.sh | bash
 
-
-
-
-
-
-
-# ---
 # Install git-completion and git-prompt
-# ---
 cd ~/
 curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
 mv ~/git-completion.bash ~/.git-completion.bash
@@ -84,9 +78,9 @@ echo "git-completion and git-prompt Installed and Configured" >> $log_file
 
 
 
-echo -e "\n====== Summary ======\n"
+echo -e "\n------ *** ------\n"
 echo
 cat $log_file
 echo
-echo -e "\n====== Summary ======\n"
+echo -e "\n------ *** ------\n"
 rm $log_file
