fe8140bc079119beb7e71848d7b0eae50751c352
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-02-24 23:26:22 +0100

    symlink

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-dotfiles=~/dotfiles
+dot=~/dotfiles
 log_file=~/install_progress_log.txt
 
-sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vim/autoload > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.SpaceVim.d > /dev/null 2>&1
+sudo rm -rf ~/.SpaceVim.d/init.toml > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.config/dunst/dunstrc > /dev/null 2>&1
+sudo rm -rf ~/.config/herbstluftwm/autostart > /dev/null 2>&1
 
-ln -sf $dotfiles/vim ~/.vim
-ln -sf $dotfiles/vim/.vimrc ~/.vimrc
-ln -sf $dotfiles/spacevim/init.toml ~/.SpaceVim.d/init.toml
-ln -sf $dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf $dotfiles/dunst/dunstrc ~/.config/dunst/dunstc
+ln -sf $dot/vim/autoload ~/.vim/autoload
+ln -sf $dot/vim/.vimrc ~/.vimrc
+ln -sf $dot/spacevim/init.toml ~/.SpaceVim.d/init.toml
+ln -sf $dot/zsh/.zshrc ~/.zshrc
+ln -sf $dot/dunst/dunstrc ~/.config/dunst/dunstc
+ln -sf $dot/herbstluftwm/autostartOrange ~/.config/herbstluftwm/autostart
