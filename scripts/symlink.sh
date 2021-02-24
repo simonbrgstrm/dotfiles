016a01fb4eec57d2a0e45156d3c3199b4931643a
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-02-25 00:20:03 +0100

    syms/inst

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -10,6 +10,8 @@ sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.config/dunst/dunstrc > /dev/null 2>&1
 sudo rm -rf ~/.config/herbstluftwm/autostart > /dev/null 2>&1
 sudo rm -rf ~/.config/polybar > /dev/null 2>&1
+sudo rm -rf ~/.config/ranger/rc.conf > /dev/null 2>&1
+
 
 ln -sf $dot/vim/autoload ~/.vim/autoload
 ln -sf $dot/vim/.vimrc ~/.vimrc
@@ -18,5 +20,7 @@ ln -sf $dot/zsh/.zshrc ~/.zshrc
 ln -sf $dot/dunst/dunstrc ~/.config/dunst/dunstc
 ln -sf $dot/herbstluftwm/autostartOrange ~/.config/herbstluftwm/autostart
 ln -sf $dot/polybar ~/.config/polybar
+ln -sf $dot/ranger/rc.confr ~/.config/ranger/rc.conf
+
 
 
