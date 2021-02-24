806270ed12a7565a5cead9afff7e7a4a4a540d00
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-02-24 23:45:13 +0100

    polysym

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -9,6 +9,7 @@ sudo rm -rf ~/.SpaceVim.d/init.toml > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.config/dunst/dunstrc > /dev/null 2>&1
 sudo rm -rf ~/.config/herbstluftwm/autostart > /dev/null 2>&1
+sudo rm -rf ~/.config/polybar > /dev/null 2>&1
 
 ln -sf $dot/vim/autoload ~/.vim/autoload
 ln -sf $dot/vim/.vimrc ~/.vimrc
@@ -16,3 +17,6 @@ ln -sf $dot/spacevim/init.toml ~/.SpaceVim.d/init.toml
 ln -sf $dot/zsh/.zshrc ~/.zshrc
 ln -sf $dot/dunst/dunstrc ~/.config/dunst/dunstc
 ln -sf $dot/herbstluftwm/autostartOrange ~/.config/herbstluftwm/autostart
+ln -sf $dot/polybar ~/.config/polybar
+
+
