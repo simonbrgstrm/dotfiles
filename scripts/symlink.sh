2304a7720fa8c438c7cc45d5683ad2b377be421a
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-02-24 22:59:00 +0100

    symlinkin

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-dotfiles_dir=~/dotfiles
+dotfiles=~/dotfiles
 log_file=~/install_progress_log.txt
 
 sudo rm -rf ~/.vim > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.SpaceVim.d > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.config/dunst/dunstrc > /dev/null 2>&1
 
-ln -sf $dotfiles_dir/vim ~/.vim
-ln -sf $dotfiles_dir/vim/vimrc ~/.vimrc
-ln -sf $dotfiles_dir/bashrc ~/.bashrc
-ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
+ln -sf $dotfiles/vim ~/.vim
+ln -sf $dotfiles/vim/.vimrc ~/.vimrc
+ln -sf $dotfiles/spacevim/init.toml ~/.SpaceVim.d/init.toml
+ln -sf $dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf $dotfiles/dunst/dunstrc ~/.config/dunst/dunstc
