bf7cfb47fd2d4ffc5b486b834277fb6929f4c586
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-03-03 20:55:34 +0100

    libs for polybar

--- install.sh
+++ install.sh
@@ -9,24 +9,45 @@
 log_file=${PWD}/installResults.txt
 
 apt=(
+  #gnome-shell-extension-pixelsaver
+  arandr
+  cmake
+  compton
+  dmenu
   docker-compose
   docker.io
   dunst
-  herbstluftwm
-  gnome-tweaks
-  rofi
-  dmenu
-  nodejs
-  gnome-shell-extension-pixelsaver
   fzf
-  npm
+  gnome-tweaks
+  herbstluftwm
+  libasound2-dev
+  libcairo2-dev
+  libcurl4-openssl-dev
+  libiw-dev
+  libmpdclient-dev
+  libpulse-dev
+  libxcb -util0-dev
+  libxcb-composite0-dev
+  libxcb-cursor-dev
+  libxcb-ewmh-dev
+  libxcb-icccm4-dev
+  libxcb-image0-dev
+  libxcb-randr0-dev
+  libxcb-xkb-dev
+  libxcb-xrm-dev
   mlocate
+  nodejs
+  npm
   python
-  python-pip
   python3
+  python3-pip
+  python3-xcbgen
   ranger
+  rofi
   spotify-client
   vim
+  xcb
+  xcb-proto
   zsh
 
 ) 
@@ -85,6 +106,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 wget -O starshipinstall.sh https://starship.rs/install.sh
 chmod +x starshipinstall.sh && ./starshipinstall.sh -f 
 rm starshipinstall.sh
+
+## Downloads polybar
+wget https://github.com/polybar/polybar/releases/download/3.5.5/polybar-3.5.5.tar.gz
+tar -C ~/ -zxvf polybar-3.5.5.tar.gz
 
 ## Print logfile
 echo -e "\n------ *** ------\n"
