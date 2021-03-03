25eb3c31eba5172f02ed8723917aaeb05287e27e
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-03-03 23:05:15 +0100

    comment

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -125,6 +125,8 @@ hc pad $monitor $panel_height
         done
         echo -n "$separator"
         echo -n "^bg()^fg() ${windowtitle//^/^^}"
+
+        ### Check path to cpu/temp/bat
 	cpu=$(( $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) /1000))
 	temp=$(( $(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
         if [ ! -f /sys/class/power_supply/BAT0/capacity ] ; then
