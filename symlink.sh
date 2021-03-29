881f70304519b9391e493a452977a9d632280766
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-03-29 09:31:42 +0200

    alac

--- symlink.sh
+++ symlink.sh
@@ -18,6 +18,7 @@ sudo rm -rf ~/.local/share/fonts > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 
 ## Create dirs and paths
+mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/dunst
 mkdir -p ~/.config/herbstluftwm
 mkdir -p ~/.config/ranger
@@ -33,39 +34,39 @@ chsh -s $(which zsh)
 #gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
 
 ## Symlink .config
-ln -sf $dot/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf $dot/.config/compton.conf ~/.config/compton.conf
-ln -sf $dot/.config/dunst/dunstrc ~/.config/dunst/dunstc
-ln -sf $dot/.config/espanso/default.yml ~/.config/espanso/default.yml
-ln -sf $dot/.config/herbstluftwm/autostartOrange ~/.config/herbstluftwm/autostart
-ln -sf $dot/.config/herbstluftwm/panel.sh ~/.config/herbstluftwm/panel.sh
-ln -sf $dot/.config/polybar ~/.config/polybar
-ln -sf $dot/.config/polybar ~/.config/polybar
-ln -sf $dot/.config/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sf $dot/.screenlayout ~/.screenlayout
-ln -sf $dot/.config/spicetify/Themes/SpicetifyDefault/color.ini ~/.config/spicetify/Themes/SpicetifyDefault/color.ini
-ln -sf $dot/.config/spicetify/Themes/SpicetifyDefault/user.css ~/.config/spicetify/Themes/SpicetifyDefault/user.css
-ln -sf $dot/.config/spicetify/config.ini ~/.config/spicetify/config.ini
-ln -sf $dot/.config/starship.toml ~/.config/starship.toml
-ln -sf $dot/.zshrc ~/.zshrc
-sudo ln -sf $dot/.config/rofi/onedark.rasi /usr/share/rofi/themes/onedark.rasi
-sudo ln -sf $dot/.config/rofi/rofi.conf /etc/rofi.conf
+ln -sf "$dot"/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf "$dot"/.config/compton.conf ~/.config/compton.conf
+ln -sf "$dot"/.config/dunst/dunstrc ~/.config/dunst/dunstc
+ln -sf "$dot"/.config/espanso/default.yml ~/.config/espanso/default.yml
+ln -sf "$dot"/.config/herbstluftwm/autostartOrange ~/.config/herbstluftwm/autostart
+ln -sf "$dot"/.config/herbstluftwm/panel.sh ~/.config/herbstluftwm/panel.sh
+ln -sf "$dot"/.config/polybar ~/.config/polybar
+ln -sf "$dot"/.config/polybar ~/.config/polybar
+ln -sf "$dot"/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf "$dot"/.screenlayout ~/.screenlayout
+ln -sf "$dot"/.config/spicetify/Themes/SpicetifyDefault/color.ini ~/.config/spicetify/Themes/SpicetifyDefault/color.ini
+ln -sf "$dot"/.config/spicetify/Themes/SpicetifyDefault/user.css ~/.config/spicetify/Themes/SpicetifyDefault/user.css
+ln -sf "$dot"/.config/spicetify/config.ini ~/.config/spicetify/config.ini
+ln -sf "$dot"/.config/starship.toml ~/.config/starship.toml
+ln -sf "$dot"/.zshrc ~/.zshrc
+sudo ln -sf "$dot"/.config/rofi/onedark.rasi /usr/share/rofi/themes/onedark.rasi
+sudo ln -sf "$dot"/.config/rofi/rofi.conf /etc/rofi.conf
 
 
 ## Symlink ./local/bin
-ln -sf $dot/.local/bin/bosse.sh ~/.local/bin/bosse
-ln -sf $dot/.local/bin/ipinfo.sh ~/.local/bin/ipinfo
-ln -sf $dot/.local/bin/sshserver.sh ~/.local/bin/sshserver
+ln -sf "$dot"/.local/bin/bosse.sh ~/.local/bin/bosse
+ln -sf "$dot"/.local/bin/ipinfo.sh ~/.local/bin/ipinfo
+ln -sf "$dot"/.local/bin/sshserver.sh ~/.local/bin/sshserver
 
 ## Fonts
-ln -sf $dot/.local/share/fonts ~/.local/share/fonts
+ln -sf "$dot"/.local/share/fonts ~/.local/share/fonts
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
 fc-cache -f -v
 
 ## SpaceVim
-ln -sf $dot/.SpaceVim.d ~/.SpaceVim.d
-ln -sf $dot/.SpaceVim.d/autoload ~/.SpaceVim.d/autoload
-ln -sf $dot/.SpaceVim.d/theme/molokai.vim ~/.SpaceVim/autoload/SpaceVim/mapping/guide/theme/molokai.vim
-ln -sf $dot/.SpaceVim.d/theme/onedark.vim ~/.SpaceVim/autoload/SpaceVim/mapping/guide/theme/onedark.vim
+ln -sf "$dot"/.SpaceVim.d ~/.SpaceVim.d
+ln -sf "$dot"/.SpaceVim.d/autoload ~/.SpaceVim.d/autoload
+ln -sf "$dot"/.SpaceVim.d/theme/molokai.vim ~/.SpaceVim/autoload/SpaceVim/mapping/guide/theme/molokai.vim
+ln -sf "$dot"/.SpaceVim.d/theme/onedark.vim ~/.SpaceVim/autoload/SpaceVim/mapping/guide/theme/onedark.vim
 
 
