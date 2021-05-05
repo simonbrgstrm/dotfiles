1b6f0a8f6613a2cfb8565ab4178ff88d08880ac6
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-05-05 21:50:00 +0200

    del

--- scripts/blue.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Change to blue theme
-
-ln -sf ~/dotfiles/herbstluftwm/autostartBlue ~/.config/herbstluftwm/autostart
-herbstclient reload
-
--- scripts/orange.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Change to orange theme
-
-ln -sf ~/dotfiles/herbstluftwm/autostartOrange ~/.config/herbstluftwm/autostart
-herbstclient reload
-
-
