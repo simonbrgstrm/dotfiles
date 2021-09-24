2fa90a4b67eedbe2605ee02bb7acfb6b49292df4
Simon Bergstrom <simonbrgstrm@gmail.com>
2021-09-24 07:29:09 +0200

    4screensNvid

---
+++ .screenlayout/4screensNvidiav2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2.1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2.3 --mode 1920x1080 --pos 4920x0 --rotate normal --output DP-0 --off --output HDMI-0 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
