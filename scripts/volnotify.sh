1e1c68b5d312d1556beace1b8660b8476a5cd7e7
Simon Bergstrom <simonbrgstrm@gmail.com>
2022-01-12 15:23:50 +0100

    vol notify-send

---
+++ scripts/volnotify.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+if [ $# -ne 1 ]; then
+    echo "Need 1 argument (volup, voldown, volmute)"
+    exit 1;
+fi
+getvol () {
+    CURVOL=$(pactl list sinks | grep RUNNING -A 8 | tail -1 | awk '{print $5}' | sed -e 's/%//')
+    if [ "$(pactl list sinks  | grep Mute | awk '{print $2}')" = 'yes' ]; then
+        echo "Muted"
+        return
+    fi
+}
+if [ "$1" = 'volup' ]; then
+    pactl set-sink-volume 0 +2%
+    MSG='Volume up'
+    getvol
+elif [ "$1" = 'voldown' ]; then
+    pactl set-sink-volume 0 -2%
+    MSG='Volume down'
+    getvol
+elif [ "$1" = 'volmute' ]; then
+    echo "Mute"
+    pactl set-sink-mute 0 toggle
+    if [ "$(pactl list sinks  | grep Mute | awk '{print $2}')" = 'yes' ]; then
+        MSG='Mute'
+    else
+        MSG='Unmute'
+    fi
+    getvol
+fi
+notify-send "$MSG ($CURVOL%)"
+# Om man vill ha nå gött sound till 
+#paplay /usr/share/sounds/freedesktop/stereo/trash-empty.oga
