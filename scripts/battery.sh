4ad0d074c9cd61be257dea5d68d72bec07b86272
Simon Bergstrom <simonbrgstrm@gmail.com>
2022-02-15 08:59:30 +0100

    change color by %

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-battery=$(cat /sys/class/power_supply/BAT0/capacity)%
-echo $battery
+battery=$(cat /sys/class/power_supply/BAT0/capacity)
+if [ "$battery" -le 15 ]
+then
+    echo "#[fg=$color_red] ${battery}%"
+elif [[ "$battery" -gt 15 && "$battery" -le 35 ]]
+then
+     echo "#[fg=$color_yellow] ${battery}%"
+else
+    echo "#[fg=$color_status_text] ${battery}%"
+fi
