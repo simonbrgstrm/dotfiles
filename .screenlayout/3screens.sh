f989fdfd2ab2bfe12002a9252eed0f2ffa3c1272
Simon Bergstrom <simonbrgstrm@gmail.com>
2023-04-18 23:14:15 +0200

    displayport

--- .screenlayout/3screens.sh
+++ .screenlayout/3screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output HDMI-0 --mode 1920x1080 --pos 1080x0 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 3000x0 --rotate normal --output DP-2 --off --output DP-2.2 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output DP-2.2 --primary --mode 1920x1080 --pos 0x0 --rotate left --output DP-0 --off --output HDMI-0 --off --output DP-1 --mode 1920x1080 --pos 3000x0 --rotate normal --output DP-2 --off --output DP-2.1 --mode 1920x1080 --pos 1080x0 --rotate normal
