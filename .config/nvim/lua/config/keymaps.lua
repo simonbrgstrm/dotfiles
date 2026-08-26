9bf2ffc2840ddedc9f3c054c345978321b5eb671
Simon Bergstrom <simonbrgstrm@gmail.com>
2026-08-26 07:54:14 +0200

    keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,8 @@ local map = vim.keymap.set
 -- buffer
 map("n", "<leader>bh", "<cmd>bprev<CR>", { desc = "Prev buffer" })
 map("n", "<leader>bl", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<leader>bc", "<cmd>bdelete<CR>", { desc = "Close buffer" })
+
+
+-- Visual-mode only: <space>/ toggles comment for the selection
+map("x", "<leader>/", "gc", { remap = true, desc = "Toggle comment selection" })
